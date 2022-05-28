f31394e161872474125b86b2ab7da11f8dc91118
토즐라 <innuendobeat@gmail.com>
2022-05-28 22:31:04 +0900

    [Bronze I] Title: 수 정렬하기, Time: 116 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n = int(input())
+    nums = []
+    for _ in range(n):
+        nums.append(int(input()))
+        
+    nums.sort()
+    for num in nums:
+        print(num)
