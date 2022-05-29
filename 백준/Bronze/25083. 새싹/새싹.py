9bbfbf404903b9702647e1642ee24a634993ca03
토즐라 <innuendobeat@gmail.com>
2022-05-29 23:33:20 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,10 @@
+def solve():
+    print("         ,r'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \\. \". L_r'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+
+if __name__ == '__main__':
+    solve()
