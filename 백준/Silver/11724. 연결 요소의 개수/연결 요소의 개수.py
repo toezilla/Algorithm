2dc500062a0aa0a5ab91ad2298b63e615ded7be9
토즐라 <innuendobeat@gmail.com>
2022-06-08 16:24:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 588 ms, Memory: 182580 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,25 +1,29 @@
-import sys
-sys.setrecursionlimit(10000)
+def dfs(start):
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i)
 
-def dfs(v):
-    visited[v] = True
-    for e in adj[v]:
-        if not visited[e]:
-            dfs(e)
-            
-N, M = map(int, input().split())
-adj = [[] for _ in range(N + 1)]
-visited = [False] * (N + 1)
-count = 0
 
-for _ in range(M):
-    u, v = map(int, input().split())
-    adj[u].append(v)
-    adj[v].append(u)
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        start, end = map(int, input().split())
+        graph[start].append(end)
+        graph[end].append(start)
+
+    visited = [False] * (1 + n)
+    cnt = 0
+
+    for i in range(1, n + 1):
+        if not visited[i]:
+            if not graph[i]:
+                cnt += 1
+                visited[i] = True
+            else:
+                dfs(i)
+                cnt += 1
+    print(cnt)
     
-for j in range(1, N + 1):
-    if not visited[j]:
-        dfs(j)
-        count += 1
-
-print(count)
