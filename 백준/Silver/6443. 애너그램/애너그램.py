0dcfae29fb8cf8c12ad2b15116bafc93943fd26e
토즐라 <innuendobeat@gmail.com>
2022-06-27 23:00:30 +0900

    [Silver I] Title: 애너그램, Time: 1020 ms, Memory: 44148 KB -BaekjoonHub

---
+++ 백준/Silver/6443. 애너그램/애너그램.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+
+def solve(l, s):
+    if l == 0:
+        visited.add(s)
+        return
+
+    for i in range(26):
+        if alpha[i] >= 1:
+            alpha[i] -= 1
+            solve(l - 1, s + chr(i + 97))
+            alpha[i] += 1
+
+for _ in range(int(stdin.readline())):
+    visited = set()
+    alpha = [0 for _ in range(26)]
+    string = stdin.readline().rstrip()
+
+    for i in string:
+        alpha[ord(i)-97] += 1
+
+    solve(len(string), '')
+    for s in sorted(list(visited)):
+        print(s)
