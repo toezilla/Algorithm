aa1b973e9bc22eeb755a52eaa4326fc2051039e2
토즐라 <innuendobeat@gmail.com>
2022-06-29 15:07:51 +0900

    [Silver III] Title: 문자열 집합, Time: 144 ms, Memory: 35784 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -2,22 +2,18 @@ import sys
 
 if __name__ == "__main__":
     input = sys.stdin.readline
-    
+
     n, m = map(int, input().split())
     answer = 0
     s_dict = {}
-    
+
     for _ in range(n):
         word = input()
-        if (first:=word[0]) in s_dict:
-            s_dict[first].append(word)
-        else:
-            s_dict[first] = [word]
-    
+        s_dict[word] = None
+
     for _ in range(m):
         compare = input()
-        if (pre:=compare[0]) in s_dict:
-            if compare in s_dict[pre]:
-                answer+=1
-    
+        if compare in s_dict:
+            answer+=1
+
     print(answer)
