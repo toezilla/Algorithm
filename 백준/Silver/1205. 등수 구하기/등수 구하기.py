d3dad32202586fdac7313913d987048cf550421c
토즐라 <innuendobeat@gmail.com>
2022-07-01 22:14:46 +0900

    [Silver V] Title: 등수 구하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,23 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    N, new, P = map(int, input().split())
+
+    score = []
+
+    if N:
+	    score = list(map(int, input().split()))
+	    score.append(new)
+	    score.sort(reverse=True)
+	    rank = score.index(new) + 1
+	    if rank > P:
+		    print(-1)
+	    else:
+		    if N == P and new == score[-1]:
+			    print(-1)
+		    else:
+			    print(rank)
+    else:
+	    print(1)
