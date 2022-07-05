bd2ada8532d07e28df14eec3499d888bb43a5574
토즐라 <innuendobeat@gmail.com>
2022-07-05 14:47:19 +0900

    [Gold III] Title: Acka, Time: 4852 ms, Memory: 100468 KB -BaekjoonHub

---
+++ 백준/Gold/12996. Acka/Acka.py
@@ -0,0 +1,25 @@
+def sol(n, a, b, c):
+    if n == 0:
+        if a == 0 and b == 0 and c == 0:
+            return 1
+        else:
+            return 0
+    if a < 0 or b < 0 or c < 0:
+        return 0
+    if dp[n][a][b][c] != -1:
+        return dp[n][a][b][c]
+    dp[n][a][b][c] = 0
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                if i + j + k == 0:
+                    continue
+                dp[n][a][b][c] += sol(n - 1, a - i, b - j, c - k)
+    dp[n][a][b][c] %= 1000000007
+    return dp[n][a][b][c]
+
+if __name__ == "__main__":
+    s, a, b, c = map(int, input().split())
+    dp = [[[[-1 for _ in range(51)] for _ in range(51)] for _ in range(51)] for _ in range(51)]
+    print(sol(s, a, b, c))
+    
