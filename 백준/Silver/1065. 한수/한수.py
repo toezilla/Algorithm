8605b77d52fde6e1e0164be080eca31babf10e35
토즐라 <innuendobeat@gmail.com>
2022-09-13 09:28:09 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,19 +1,22 @@
-def hansoo(N):
-    answer_list = []
-    for i in range(1, N+1):
-        number_list = list(map(int, str(i)))
-        
-        if 1 <= i < 10:
-            answer_list.append(0)
-        elif 10 <= i < 100:
-            answer_list.append(1)
-        elif 100 <= i < 1000:
-            if number_list[1] - number_list[0] == number_list[2] - number_list[1]:
-                answer_list.append(2)
-            else:
-                pass
-        else:
-            pass
-    print(len(answer_list))
-            
-hansoo(int(input()))
+def hansoo(number: int):
+    answer = 0
+    number = str(number)
+    # if number < 100
+    if len(number) <= 2:
+        answer = int(number)
+        return answer
+
+    # if number >= 100
+    answer += 99
+    for num in range(100, int(number)+1):
+        num = str(num)
+        diff = int(num[0]) - int(num[1])
+        for i in range(1, len(num)-1):
+            if int(num[i]) - int(num[i+1]) == diff:
+                answer += 1
+    return answer
+
+
+if __name__ == "__main__":
+    number = int(input())
+    print(hansoo(number))
