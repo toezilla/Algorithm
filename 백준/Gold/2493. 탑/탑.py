c287f207635c9da34b98e25a27f5766c31aba577
토즐라 <innuendobeat@gmail.com>
2022-09-26 09:06:02 +0900

    [Gold V] Title: 탑, Time: 664 ms, Memory: 125640 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,16 +1,20 @@
-n = int(input())
-a = list(map(int, input().split()))
+if __name__ == "__main__":
+    n = int(input())
+    a = list(map(int, input().split()))
 
-stack = []
-res = [0]*n
+    stack = []
+    answer = [0] * n
 
-for i in enumerate(a):
-    idx = i[0]
-    while stack:
-        if i[1] >= stack[-1][1]:
-            stack.pop()
-        else:
-            res[idx] = stack[-1][0]+1
+    for i in enumerate(a):
+        idx = i[0]
+        while stack:
+            if i[1] >= stack[-1][1]:
+                stack.pop()
+                continue
+            answer[idx] = stack[-1][0]+1
             break
-    stack.append(i)
-print(*res)
+            
+        stack.append(i)
+        
+    print(*answer)
+    
